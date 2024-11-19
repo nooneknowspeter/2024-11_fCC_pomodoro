cf6dd7cc4cea0e9319dae907c230d379beba8a65
peter <jerelepeter06@gmail.com>
2024-11-19 15:57:03 -0400

    added background and created main timer assembly using concept from Figma turned those components and added back to main App function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
+import { FaPause, FaUndoAlt } from "react-icons/fa";
+import { IoSettingsSharp } from "react-icons/io5";
+
+const Background = () => {
+  return (
+    <div
+      id="background"
+      className="absolute left-0 top-0 h-screen w-screen bg-neutral-950"
+    ></div>
+  );
+};
+
+const MainTimer = () => {
+  return (
+    <div id="main-timer" className="flex flex-col gap-4 lg:w-[200px]">
+      <h1 id="timer-label" className="font-bold">{`Session`}</h1>
+      <h1 id="timer-left" className="font-normal">{`25:00`}</h1>
+      <div
+        id="controls"
+        className="flex flex-row place-content-center content-center gap-10"
+      >
+        <FaPause />
+        <FaUndoAlt />
+        <IoSettingsSharp />
+      </div>
+      <div
+        id="progress-bar"
+        className="h-2 w-full rounded-sm bg-neutral-50"
+      ></div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <>
-      <div id="background" className=""></div>
-      <div id="pomodoro-assembly"></div>
+      <Background />
+      <div
+        id="pomodoro-assembly"
+        className="relative flex h-screen select-none flex-row place-content-center items-center justify-center text-center text-xl text-neutral-50"
+      >
+        <MainTimer />
+      </div>
     </>
   );
 };
