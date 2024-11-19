3f4426e6de1b92a201040a54a849c475eda835b0
peter <jerelepeter06@gmail.com>
2024-11-19 16:10:40 -0400

    created set timer component from concept going to implement it into an internal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FaPause, FaUndoAlt } from "react-icons/fa";
+import { FaArrowDown, FaArrowUp, FaPause, FaUndoAlt } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
 
 const Background = () => {
@@ -37,9 +37,29 @@ const App = () => {
       <Background />
       <div
         id="pomodoro-assembly"
-        className="relative flex h-screen select-none flex-row place-content-center items-center justify-center text-center text-xl text-neutral-50"
+        className="relative flex h-screen select-none flex-col place-content-center items-center justify-center text-center text-xl text-neutral-50"
       >
-        <MainTimer />
+        {/* <MainTimer /> */}
+        <div
+          id={`set-${`NameOfTimer`}-timer`}
+          className="flex flex-col gap-4 lg:w-[200px]"
+        >
+          <h1
+            id={`${`NameOfTimer`}-label`}
+            className="font-bold"
+          >{`NameOfTimer`}</h1>
+          <div
+            id={`${`NameOfTimer`}-controls`}
+            className="flex flex-row place-content-center content-center gap-10 text-center"
+          >
+            <FaArrowUp id={`${`NameOfTimer`}-increment`} />
+            <h1
+              id={`${`NameOfTimer`}-length`}
+              className="font-normal"
+            >{`05:00`}</h1>
+            <FaArrowDown id={`${`NameOfTimer`}-decrement`} />
+          </div>
+        </div>
       </div>
     </>
   );
