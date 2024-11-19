ba8191b943014f44f5d86721021b4a8b58813147
peter <jerelepeter06@gmail.com>
2024-11-19 16:37:36 -0400

    got the main timer to stay in the center using absolute and constraints the set timer components set horizontally / adjacent to the main timer in a flex row might change it to make it more responsive for mobile let's see

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const MainTimer = () => {
   return (
     <div
       id="main-timer"
-      className="flex h-screen origin-center flex-col place-content-center justify-center gap-4 lg:w-[200px]"
+      className="absolute bottom-0 left-0 right-0 top-0 mx-auto flex origin-center flex-col place-content-center items-center justify-center gap-4 lg:w-[200px]"
     >
       <h1 id="timer-label" className="font-bold">{`Session`}</h1>
       <h1 id="timer-left" className="font-normal">{`25:00`}</h1>
@@ -65,14 +65,14 @@ const App = () => {
       <Background />
       <div
         id="pomodoro-assembly"
-        className="relative flex h-screen select-none flex-col place-content-center items-center justify-center gap-10 text-center text-xl text-neutral-50"
+        className="relative flex h-screen origin-center select-none flex-col place-content-center items-center justify-center text-center text-xl text-neutral-50"
       >
         <MainTimer />
 
-        {/* <div id="set-timers" className="flex flex-row gap-96">
+        <div id="set-timers" className="flex flex-row gap-96">
           <SetTimer />
           <SetTimer />
-        </div> */}
+        </div>
       </div>
     </>
   );
