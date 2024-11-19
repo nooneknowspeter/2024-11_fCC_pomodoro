00501f46dc42c6e4b2a0ea6a1c88c84bfe0d3775
peter <jerelepeter06@gmail.com>
2024-11-19 15:27:39 -0400

    intalled tailwind prettier plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <div id="background" className=""></div>
+      <div id="pomodoro-assembly"></div>
     </>
   );
 };
