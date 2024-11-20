9fdf7d0d452f9a695141364b23cf73250549fc1c
peter <jerelepeter06@gmail.com>
2024-11-19 20:06:00 -0400

    npm i react-animated=curesor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AnimatedCursor from "react-animated-cursor";
 import { FaArrowDown, FaArrowUp, FaPause, FaUndoAlt } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
 
@@ -172,6 +173,27 @@ const App = () => {
           onClickIncrement={setSessionTimer}
         />
       </div>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={8}
+        color="193, 11, 111"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
     </>
   );
 };
