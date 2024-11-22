4c86285aa19d1dc09d25dc0c13a449748836a942
peter <jerelepeter06@gmail.com>
2024-11-21 21:47:21 -0400

    installed glsify, vite-glsl, still trying to configure glsl

--- src/components/Background/BackgroundAssembly.tsx
+++ src/components/Background/BackgroundAssembly.tsx
@@ -1,25 +1,9 @@
-import { Canvas } from "@react-three/fiber";
-
-const Mesh = (props: { height: number; width: number }) => {
-  return (
-    <mesh>
-      <planeGeometry args={[props.height, props.width]} />
-    </mesh>
-  );
-};
-
 const BackgroundAssembly = () => {
   return (
     <div
       id="background"
       className="absolute left-0 top-0 h-screen w-screen bg-neutral-950"
-    >
-      <Canvas>
-        <Mesh height={window.innerHeight} width={window.innerWidth} />
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} color="white" />
-      </Canvas>
-    </div>
+    ></div>
   );
 };
 
